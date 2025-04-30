50be1b738a7e3c0c0d60a3b1ddab7eaba0862725
Kiady <rambelosonkiady@gmail.com>
2025-04-30 16:55:17 +0300

    chore: remove unused image assets and update homepage layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
 <div class="">
-	<h1 class="text-red-600">Hello world</h1>
+	<div class="flex bg-red-500 h-[100vh] items-center">
+		<div class="w-1/3">
+			<h1 class="text-5xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde qui commodi cumque, pariatur fugit officia saepe eligendi illum. Possimus, laudantium?</h1>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, eos?</p>
+		</div>
+		<div class="w-1/3">
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos est perferendis corrupti adipisci incidunt nesciunt, ea tenetur quasi magnam ducimus.</p>
+		</div>
+	</div>
+	<div class="">
+		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, natus.</p>
+	</div>
 </div>
 
 <style>
