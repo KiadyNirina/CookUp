34d5db42035f53e7e796bef1c413b2ba2dd5091f
Kiady <rambelosonkiady@gmail.com>
2025-05-27 17:05:13 +0300

    feat: enhance layout responsiveness with media queries and improve section styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,19 +52,19 @@
 	</div>
 	{/if}
 	<div class="h-[100vh]">
-		<div class="flex h-full items-center">
-			<div class="w-1/2">
+		<div class="header flex h-full items-center">
+			<div class="sect1 w-1/2">
 				<h1 class="text-8xl font-extrabold">Fini le casse-tête des repas.</h1>
 				<p class="dark:font-thin mt-5">Choisissez un critère, on s’occupe du reste.</p>
 				<button
-					class="mt-5 flex items-center bg-yellow-600 text-white dark:text-black font-bold p-4 rounded-full transition-all duration-300 ease-in-out hover:cursor-pointer hover:text-yellow-600 hover:bg-transparent border-2 hover:border-yellow-600 active:scale-70"
+					class="button mt-5 flex items-center bg-yellow-600 text-white dark:text-black font-bold p-4 rounded-full transition-all duration-300 ease-in-out hover:cursor-pointer hover:text-yellow-600 hover:bg-transparent border-2 hover:border-yellow-600 active:scale-70"
 					on:click={togglePoppup}
 				>
 					<Icon icon="mdi:timer-outline" class="mr-1" />
 					C’est parti !
 				</button>
 			</div>
-			<div class="w-1/2">
+			<div class="sect2 w-1/2">
 				<img
 					src="/img/breakfast.svg"
 					alt="Petit déj"
@@ -73,7 +73,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="text-sm text-center p-2 text-gray-600 dark:text-gray-400">
+	<div class="footer text-sm text-center p-2 text-gray-600 dark:text-gray-400">
 		<p>© 2025 by <a href="https://kiadynirina.netlify.app/" target="_blank">KiadyN</a>. Tous droits réservés.</p>
 	</div>
 </div>
@@ -96,5 +96,55 @@
 	}
 	.logo {
 		font-family: "Pacifico";
+	}
+
+	@media screen and (max-width: 640px) {
+		.header {
+			flex-direction: column;
+			justify-content: center;
+		}
+		.sect1 {
+			width: 100%;
+			text-align: center;
+		}
+		.sect2 {
+			margin-top: 20px;
+			width: 100%;
+			text-align: center;
+		}
+		.sect2 img {
+			width: 90%;
+			margin-left: auto;
+			margin-right: auto;
+		}
+		.sect1 h1 {
+			font-size: 3rem;
+		}
+		.sect1 p {
+			font-size: 12px;
+		}
+		.button {
+			margin-left: auto;
+			margin-right: auto;
+			font-size: 12px;
+		}
+		.footer p {
+			font-size: 10px;
+		}
+	}
+
+	@media screen and (min-width: 641px) and (max-width: 768px) {
+		.sect1 h1 {
+			font-size: 3rem;
+		}
+		.sect1 p {
+			font-size: 15px;
+		}
+		.button {
+			font-size: 12px;
+		}
+		.footer p {
+			font-size: 10px;
+		}
 	}
 </style>
